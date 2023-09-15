fb7fb3ed530dbbb611d8d303546484307a1677ce
Anastasiia Shumeiko <anastasiia.shumeiko@macaw.nl>
2023-09-15 13:51:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
             <footer>
-              This project was coded by Anastasiia Shumeiko and is 
-            <a href="https://github.com/AnastasiaCodeGirl/react-weather-app" target="_blank"></a> 
-            open-ourced on GitHub
+              This project was coded by Anastasiia Shumeiko and is {" "}
+            <a href="https://github.com/AnastasiaCodeGirl/react-weather-app" target="_blank">open-sourced on GitHub</a> 
+            
             </footer>
     </div>
 
