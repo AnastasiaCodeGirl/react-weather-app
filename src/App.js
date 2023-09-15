9cb635fe0b1e302bbad6edc5e8d085d2ca619a3c
Anastasiia Shumeiko <anastasiia.shumeiko@macaw.nl>
2023-09-15 13:47:26 +0200

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Weather App
+      <h1>Weather App</h1>
+            <footer>
+              This project was coded by Anastasiia Shumeiko and is 
+            <a href="https://github.com/AnastasiaCodeGirl/react-weather-app" target="_blank"></a> 
+            open-ourced on GitHub
+            </footer>
     </div>
+
   );
 }
 
